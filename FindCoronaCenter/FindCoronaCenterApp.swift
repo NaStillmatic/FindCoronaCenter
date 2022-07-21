6094d8ed90edea045570590a22b1ccbae220fbbe
Joe <nastillmatic@gmail.com>
2022-07-21 23:35:22 +0900

    initial commit

---
+++ FindCoronaCenter/FindCoronaCenterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindCoronaCenterApp.swift
+//  FindCoronaCenter
+//
+//  Created by HwangByungJo  on 2022/07/21.
+//
+
+import SwiftUI
+
+@main
+struct FindCoronaCenterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
